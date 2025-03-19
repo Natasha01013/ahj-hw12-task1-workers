11f4ed98dc46023fdf82dee7be6e74bd0aa8b302
Natasha01013 <natasha01013@yandex.ru>
2025-03-19 12:06:37 +0100

    change index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style.css';
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register('./sw.js');
   });
 }
 
